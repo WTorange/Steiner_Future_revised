8f85f786012439d3cc6c058d3b9fbec738002d61
WTorange <152046180+WTorange@users.noreply.github.com>
2024-10-16 10:10:46 +0800

    last version

--- get_3essential_contracts.py
+++ get_3essential_contracts.py
@@ -8,7 +8,12 @@ warnings.filterwarnings('ignore')
 pd.set_option('display.max_columns', None)
 
 class get_essential_contract(ConnectDatabase):
-
+    '''
+    根据期货品种，从wind的CCOMMODITYFUTURESEODPRICES, CINDEXFUTURESEODPRICES, CBONDFUTURESEODPRICES表格中获取日频数据，
+    根据持仓量和交易量、到期日期获取前三个主力合约，和最近月合约。分品种输出每天的四类合约数据到\\samba1.quantchina.pro\quanyi4g\temporary\Steiner\data_wash\linux_so\py311\essentialcontract文件夹
+    需要from connect_wind import ConnectDatabase，读取wind数据
+    目前是根据\\samba1.quantchina.pro\quanyi4g\temporary\Steiner\data_wash\linux_so\py311\品种列表.csv 获取
+    '''
     def __init__(self, future_symbol, type: str, start_date: str, end_date: str):
         self.future_symbol = future_symbol  #例 铜CU(大写)
         self.type = type #com商品， ind股指， bond国债
@@ -20,9 +25,9 @@ class get_essential_contract(ConnectDatabase):
             self.table = 'CINDEXFUTURESEODPRICES'
         elif type == 'bond':
             self.table = 'CBONDFUTURESEODPRICES'
-# 这里不对，应该用S_DQ_OI 持仓量（手）。确实是用日行情数据。遍历交易日期————找到所有的对应品种的合约代码————找到持仓量最大的前三名合约，但是没有到七月份（可以通过合约代码的数字判断，数字大的是远月合约）。
+
         self.sql = f'''
-                    SELECT S_INFO_WINDCODE, TRADE_DT, S_DQ_OI
+                    SELECT S_INFO_WINDCODE, TRADE_DT, S_DQ_OI, S_DQ_VOLUME
                     FROM {self.table}
                     WHERE TRADE_DT between {start_date} and {end_date}
                     AND S_INFO_WINDCODE LIKE '{future_symbol}%'
@@ -33,22 +38,57 @@ class get_essential_contract(ConnectDatabase):
         # self.df.sort_values(by='date', inplace=True)
         # self.df.reset_index(drop=True, inplace=True)
 
+    def correct_czc_code(self, contract, query_date):
+        if contract.endswith('.CZC'):
+            # 提取期货代码中的字母和数字部分
+            match = re.match(r'^([A-Z]+)(\d+)\.CZC$', contract)
+            if match:
+                letters, numbers = match.groups()
+                # 提取查询时间的年份
+                string_date = query_date.strftime('%Y')
+                year = int(string_date)
+                # year = int(query_date[:4])
+                # 修正数字部分，在第一位加上年份
+                if len(numbers) == 3 and numbers[0] != '9' and year >= 2019:
+                    corrected_numbers = '2' + numbers
+                elif len(numbers) == 3:
+                    corrected_numbers = '1' + numbers
+                else:
+                    corrected_numbers = numbers
+                return f"{letters}{corrected_numbers}.CZC"
+        if contract.endswith('.GFE'):
+            corrected_contract = contract.replace('.GFE', '.GFEX')
+            return corrected_contract
+        return contract
+
     def process(self):
         # 将 S_DQ_OI 转换为数值型
         self.df['S_DQ_OI'] = pd.to_numeric(self.df['S_DQ_OI'], errors='coerce')
-        mask = self.df['S_INFO_WINDCODE'].str.match(f'^({"|".join(symbol_list)})\d{{3,4}}\.\w+$')
+        self.df['S_DQ_VOLUME'] = pd.to_numeric(self.df['S_DQ_OI'], errors='coerce')
+        mask = self.df['S_INFO_WINDCODE'].str.match(f"^{re.escape(self.future_symbol)}\\d{{3,4}}\\..*$")
         filtered_df = self.df[mask]
-        # print(filtered_df)
-        # top3_per_day = filtered_df.groupby('TRADE_DT').apply(lambda x: x.nlargest(3, 'S_DQ_OI')).reset_index(drop=True)
+        self.df['TRADE_DT'] = pd.to_datetime(self.df['TRADE_DT'], format='%Y%m%d')
+        filtered_df['TRADE_DT'] = pd.to_datetime(self.df['TRADE_DT'], format='%Y%m%d')
+        # filtered_df.to_csv('check.csv')
+        print(filtered_df['S_INFO_WINDCODE'])
+        if filtered_df is None or filtered_df.empty or filtered_df['S_INFO_WINDCODE'].empty or filtered_df['S_INFO_WINDCODE'].isna().all():
+            return None
+        filtered_df['S_INFO_WINDCODE'] = filtered_df.apply(
+            lambda row: self.correct_czc_code(row['S_INFO_WINDCODE'], row['TRADE_DT']), axis=1)
 
-        top3_per_day = filtered_df.sort_values(by=['S_DQ_OI', 'S_INFO_WINDCODE'], ascending=[False, False]) \
+
+
+        top3_per_day = filtered_df.sort_values(by=['S_DQ_OI', 'S_DQ_VOLUME'], ascending=[False, False]) \
                                  .groupby('TRADE_DT') \
                                  .apply(lambda x: x.head(3)) \
                                  .reset_index(drop=True)
+        filtered_df['contract_num'] = filtered_df['S_INFO_WINDCODE'].str.extract(r'(\d+)').astype(int)
+        nearby_contracts = filtered_df.loc[filtered_df.groupby('TRADE_DT')['contract_num'].idxmin()][['TRADE_DT', 'S_INFO_WINDCODE']]
 
-        output_df = pd.DataFrame(columns=['future_code', 'date', 'main_contract', 'second', 'third'])
+        output_df = pd.DataFrame(columns=['future_code', 'date', 'main_contract', 'second', 'third','nearby_contract'])
         for date, group in top3_per_day.groupby('TRADE_DT'):
             top3_codes = list(group['S_INFO_WINDCODE'].iloc[:3])
+            nearby_contract = nearby_contracts.loc[nearby_contracts['TRADE_DT'] == date, 'S_INFO_WINDCODE'].values[0]
             output_df = pd.concat([
                 output_df,
                 pd.DataFrame({
@@ -56,7 +96,8 @@ class get_essential_contract(ConnectDatabase):
                     'date': [date],
                     'main_contract': [top3_codes[0] if len(top3_codes) > 0 else None],
                     'second': [top3_codes[1] if len(top3_codes) > 1 else None],
-                    'third': [top3_codes[2] if len(top3_codes) > 2 else None]
+                    'third': [top3_codes[2] if len(top3_codes) > 2 else None],
+                    'nearby_contract': [nearby_contract]
                 })
             ], ignore_index=True)
 
@@ -71,12 +112,12 @@ class get_essential_contract(ConnectDatabase):
 
     def run(self):
         df = self.process()
-        df['future'] = self.future_symbol
+        # df['future'] = self.future_symbol
 
         return df
 
 if __name__ == '__main__':
-    symbol_df = pd.read_csv(r"C:\Users\maki\Desktop\quantchina\Futures-main\data_wash\品种列表.csv")
+    symbol_df = pd.read_csv(r'\\samba-1.quantchina.pro\quanyi4g\temporary\Steiner\data_wash\linux_so\py311\品种列表.csv')
 
     # 提取品种代码并去除.后的字符
     symbol_list = symbol_df['S_INFO_WINDCODE'].apply(lambda x: x.split('.')[0]).tolist()
@@ -87,6 +128,7 @@ if __name__ == '__main__':
     #
     for type in ['com', 'ind', 'bond']:
         for symbol in symbol_list:
+            print(symbol)
             mc = get_essential_contract(symbol, type, start_date=start_date, end_date=end_date)
             temp = mc.run()
             print(temp)
@@ -99,12 +141,6 @@ if __name__ == '__main__':
             temp.to_csv(file_path, index=False)
             print(f"Saved file: {file_path}")
 
-
-    # get2 = get_essential_contract('IM', 'ind', '20230103', '20230110')
-    # print(get2.run())
-
-    # get3= get_essential_contract('T', 'bond', '20230103', '20230110')
-    # print(get3.run())
 
 
 
